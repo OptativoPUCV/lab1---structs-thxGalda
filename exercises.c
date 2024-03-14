e5bae17a29b57139c846e7ac081b3cf34c6360fe
Your Name <you@example.com>
2024-03-14 21:00:32 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -50,7 +50,7 @@ int *filterEvenNumbers(int arr[], int size, int *newSize)
   for (int i = 0; i < size; i++)
     {
       if (arr[i] % 2 == 0) newArr[*newSize] = arr[i];
-      *newSize++;
+      (*newSize)++;
     }
   return newArr; 
 }
