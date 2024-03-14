f650f67ef927a33bde14f6c0f786f70e4382dd1b
Your Name <you@example.com>
2024-03-14 20:57:02 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -70,7 +70,8 @@ int compare(const void *pivot, const void *elem)
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[])
 {
   int size3 = size1 + size2;
-  for (int i = 0; i < size1; i++)
+  int i;
+  for (i = 0; i < size1; i++)
     {
       result[j] = arr1[i];
     }
